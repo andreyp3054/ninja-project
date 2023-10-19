c2b14dcf2b5620500ee0a120e318386006c58e4d
Mark Andrey Acebu <markandrey.acebu@gmail.com>
2023-10-19 22:29:30 +0800

    problem: fetching picture from pubspec.yaml

---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MaterialApp(home: NinjaCard()));
+
+class NinjaCard extends StatelessWidget {
+  const NinjaCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: const Text("Dev ID Card"),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage("assets/ninjaID.jpg"),
+                radius: 40,
+              ),
+            ),
+            Divider(
+              height: 60,
+              color: Colors.grey[800],
+            ),
+            const Text(
+              "NAME",
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              "Mark Andrey Acebu",
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 30),
+            const Text(
+              "CURRENT DEV LEVEL",
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              "1 ",
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 30),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400],
+                ),
+                const SizedBox(width: 10),
+                Text(
+                  "markandrey.acebu@gmail.com",
+                  style: TextStyle(
+                      color: Colors.grey[400],
+                      letterSpacing: 1.0,
+                      fontSize: 18),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
